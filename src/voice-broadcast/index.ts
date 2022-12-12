34b8a7bd92e56c6e85c1376e692e68f91580e6ed
Michael Weimann <michaelw@matrix.org>
2022-12-12 15:56:06 +0100

    When deleting a voice broadcast, also delete related events

--- src/voice-broadcast/index.ts
+++ src/voice-broadcast/index.ts
@@ -47,6 +47,7 @@ export * from "./utils/doMaybeSetCurrentVoiceBroadcastPlayback";
 export * from "./utils/getChunkLength";
 export * from "./utils/getMaxBroadcastLength";
 export * from "./utils/hasRoomLiveVoiceBroadcast";
+export * from "./utils/isVoiceBroadcastStartedEvent";
 export * from "./utils/findRoomLiveVoiceBroadcastFromUserAndDevice";
 export * from "./utils/shouldDisplayAsVoiceBroadcastRecordingTile";
 export * from "./utils/shouldDisplayAsVoiceBroadcastTile";
